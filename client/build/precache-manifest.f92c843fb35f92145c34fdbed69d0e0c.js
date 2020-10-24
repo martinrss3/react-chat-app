c53b1224d67a880477b2c8eeba9a871c25d5b436
martin <martin@kali.kali>
2020-10-24 01:30:56 -0300

    first commit

---
+++ client/build/precache-manifest.f92c843fb35f92145c34fdbed69d0e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7b49e91dd5161aba72da574e8faaf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c8a72f57d50fb88d0bf",
+    "url": "/static/css/main.a12686a1.chunk.css"
+  },
+  {
+    "revision": "311225b53ea0510d488a",
+    "url": "/static/js/2.31ab4d1d.chunk.js"
+  },
+  {
+    "revision": "c69fcf8670b313a5daf5b087e062c182",
+    "url": "/static/js/2.31ab4d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8a72f57d50fb88d0bf",
+    "url": "/static/js/main.3524f0db.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
